fe8ffc3caac8c751424c8ef94850530b93a2d357
BorisSiman <boris.siman@devtechgroup.com>
2018-03-22 12:58:22 +0100

    styling finish button

--- src/components/Tasks/Task/Task.js
+++ src/components/Tasks/Task/Task.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
 
-const task = (props) => {    
+
+const task = (props) => {
+let btnFinish = 'Finish';
+const styling = {backgroundColor: ''};
+if(props.done){
+    btnFinish = 'Undo'
+    styling.backgroundColor= '#3a7a82';
+}    
     return (
-        <div className='container border border-white rounded w-75 p-3 my-3' style={{backgroundColor: props.done ? '#428bca' : ''}}>
+        <div className='container border border-white rounded w-75 p-3 my-3' style={{backgroundColor: props.done ? '#3a7a82' : ''}}>
     <div className ='row'>
     <div 
         className='col-md-6 text-white lead pt-2' 
@@ -12,7 +19,7 @@ const task = (props) => {
     </div>
     <div className='col-md-6 text-right'>
     <button className='btn btn-danger m-1' onClick={props.click}> Delete task </button>
-    <button className='btn btn-primary m-1' onClick={props.finish}>Finish task</button> </div>
+    <button className='btn border-white btn-info text-white m-1' style={styling} onClick={props.finish}>{btnFinish}</button> </div>
       </div>
       </div>
     )
